b6ac05a367dd38b5d4e3d2fc2c11ee284dd9edf7
Hellyes31 <elies.auguet@gmail.com>
2025-06-17 15:42:02 +0200

    Update avec Etape 3

--- 02guess_a_number/ExGuessANumber.js
+++ 02guess_a_number/ExGuessANumber.js
@@ -3,4 +3,67 @@ function askNumber(){
 }
 let result = askNumber()
 console.log(result)
-// let givenNumber = window.prompt("Donnez moi un nombre")
+
+
+function didIWin(givenNumber){
+  if (givenNumber < 22){
+    alert("Plus Grand") 
+    return  false
+  }
+  else if (givenNumber > 22){
+    alert("Plus Petit") 
+    return  false
+  }
+  else {
+    alert("Bravo ! Vous avez deviné le nombre") 
+    return  true
+}
+}
+// alert(didIWin(result))
+
+
+// ETAPE 3 :
+
+function gamePlay() {
+  while (didIWin(result) == false){
+    alert("RECOMMEEEEEEENNNNNCEEEEEE!!!")
+    result = askNumber()
+  }
+}
+gamePlay()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function gamePlay() {
+//   let hasWon = false;
+
+//   while (!hasWon) {
+//     let userInput = prompt("Devinez le nombre entre 1 et 100 :");
+//     let guess = parseInt(userInput);
+
+//     if (isNaN(guess)) {
+//       alert("Veuillez entrer un nombre valide !");
+//       continue;
+//     }
+
+//     hasWon = didIWin(guess);
+//   }
+// }
+// gamePlay()
+// Désormais la fonction didIWin devra retourner true si l’utilisateur a trouvé le chiffre, false sinon.
+// Dans la fonction gamePlay, si didIWin a retourné true, on arrete le jeu. En revanche, si elle a retourné false, on redemande un chiffre à l’utilisateur.
+
