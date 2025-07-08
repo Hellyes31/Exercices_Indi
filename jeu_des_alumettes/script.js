8a98efdfe07e32e5c4a54de804d0ab3f3d7ec859
Hellyes31 <elies.auguet@gmail.com>
2025-07-08 14:34:28 +0200

    Etape4,2joueurs

--- jeu_des_alumettes/script.js
+++ jeu_des_alumettes/script.js
@@ -5,16 +5,43 @@
 // Faire une fonction qui prend en paramètre le nombre d'allumettes à enlever du reste.
 // Pour rappel, une nom de fonction doit décrire clairement ce que fait celle-ci.
 
+// function getMatches1() {
+//     let promptNumber2 = prompt("Combien d'alumettes souhaites-tu retirer ?")
+
+//     if (Number(promptNumber2) >= 1 && Number(promptNumber2) <= 6){
+//         if(currentPlayer == 2){
+//         matches = matches - Number(promptNumber2)
+//         alert("Il reste " + matches + " allumettes.")
+//         currentPlayer -=1
+//         gameMatches()
+//         }
+//     } else {
+//         alert("Saisissez un chiffre adéquat entre 1 et 6")
+//         alert("Il reste " + matches + " allumettes.")
+//         getMatches1()
+//     }
+// }
+
+
 let matches = 50
-
-
+let currentPlayer = 1
+let loser = currentPlayer === 1 ? 2 : 1
 function getMatches() {
-    let promptNumber = prompt("Combien d'alumettes souhaites-tu retirer ?")
-    
-    if (Number(promptNumber) >= 1 && Number(promptNumber) <= 6){
-        matches = matches - Number(promptNumber)
+    let promptNumber1 = prompt("Joueur : " + currentPlayer + ". " + "Combien d'alumettes souhaites-tu retirer ?")
+
+    if (Number(promptNumber1) >= 1 && Number(promptNumber1) <= 6){
+        if(currentPlayer == 1){
+        matches = matches - Number(promptNumber1)
         alert("Il reste " + matches + " allumettes.")
+        currentPlayer += 1
         gameMatches()
+        }
+        else if (currentPlayer ==2){
+        matches = matches - Number(promptNumber1)
+        alert("Il reste " + matches + " allumettes.")
+        currentPlayer -=1
+        gameMatches()
+        }
     } else {
         alert("Saisissez un chiffre adéquat entre 1 et 6")
         alert("Il reste " + matches + " allumettes.")
@@ -24,14 +51,16 @@ function getMatches() {
 
 function gameMatches () {
     if (matches < 0) {
-        alert("Perdu ! Les allumettes ne peuvent pas être en négatif.")
+        alert("Perdu !"  + "Les allumettes ne peuvent pas être en négatif. Joueur "+ currentPlayer + " a gagné !" )
     } else if (matches === 0) {
-        alert("Félicitations ! Tu as gagné !")
+        alert("Félicitations joueur " + loser + " ! " + "Tu as gagné !")
     } else {
         getMatches()
     }
 }
 
 gameMatches() // ✅ Lancement du jeu
+
+
 
 
