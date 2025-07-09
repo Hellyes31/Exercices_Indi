41852ce1c890612ab882bf95752ba9ac7538a4ea
Hellyes31 <elies.auguet@gmail.com>
2025-07-09 10:51:42 +0200

    Multijoueur manque CSS

--- jeu_des_alumettes/script.js
+++ jeu_des_alumettes/script.js
@@ -22,44 +22,101 @@
 //     }
 // }
 
+//*********************************************************************************************************************************** */
 
-let matches = 50
+
+//***************************2 joueurs ***************************/
+
+// function getMatches() {
+//     let promptNumber1 = prompt("Joueur : " + currentPlayer + ". " + "Combien d'alumettes souhaites-tu retirer ?")
+
+//     if (Number(promptNumber1) >= 1 && Number(promptNumber1) <= 6){
+//         if(currentPlayer == 1){
+//         matches = matches - Number(promptNumber1)
+//         alert("Il reste " + matches + " allumettes.")
+        
+//         // currentPlayer = (currentPlayer % totalPlayers) + 1;
+//         gameMatches()
+//         currentPlayer +=1
+//         if (matches > 0){
+//         getMatches()
+//         }
+//         }
+//         else if (currentPlayer ==2){
+//         matches = matches - Number(promptNumber1)
+//         alert("Il reste " + matches + " allumettes.")
+//         gameMatches()
+//         currentPlayer -=1
+//         if (matches > 0){
+//         getMatches()
+//         }
+//         }
+//     } else {
+//         alert("Saisissez un chiffre adéquat entre 1 et 6")
+//         alert("Il reste " + matches + " allumettes.")
+//         getMatches()
+//     }
+// }
+// function gameMatches () {
+//     if (matches < 0) {
+//         alert("Perdu !"  + "Les allumettes ne peuvent pas être en négatif. Joueur "+ currentPlayer + " a gagné !" )
+//     } else if (matches === 0) {
+//         alert("Félicitations joueur " + currentPlayer + " ! " + "Tu as gagné !")
+//     }
+// }
+
+// getMatches()
+
+
+
+//*********************************************************************************************************************************** */
+let matches = 50 
 let currentPlayer = 1
-let loser = currentPlayer === 1 ? 2 : 1
+let totalPlayer = ""
+function multiplayer(){
+    totalPlayer = Number(prompt("Combien de joueur êtes vous ?"))
+    if (totalPlayer >=2){
+        alert("Nombre de joueurs : " + totalPlayer)
+    }
+    else {
+        alert("Veuillez donner un nombre adéquat, le nombre de joueur minimum est 2 !")
+        multiplayer()
+    }
+}
+multiplayer()
+
 function getMatches() {
-    let promptNumber1 = prompt("Joueur : " + currentPlayer + ". " + "Combien d'alumettes souhaites-tu retirer ?")
+    for (let i=1; i<=totalPlayer; i++){
+        if(matches <= 0){
+            break
+        }
+        promptNumber1 = prompt("Joueur : " + i + ". " + "Combien d'alumettes souhaites-tu retirer ?")
+        if (Number(promptNumber1) >= 1 && Number(promptNumber1) <= 6){
+            matches = matches - Number(promptNumber1)
+            alert("Il reste " + matches + " allumettes.")
+            gameMatches(i)
+        }
+        else {
+            alert("Saisissez un chiffre adéquat entre 1 et 6")
+            alert("Il reste " + matches + " allumettes.")
+            i--
+        }
+    }
 
-    if (Number(promptNumber1) >= 1 && Number(promptNumber1) <= 6){
-        if(currentPlayer == 1){
-        matches = matches - Number(promptNumber1)
-        alert("Il reste " + matches + " allumettes.")
-        currentPlayer += 1
-        gameMatches()
-        }
-        else if (currentPlayer ==2){
-        matches = matches - Number(promptNumber1)
-        alert("Il reste " + matches + " allumettes.")
-        currentPlayer -=1
-        gameMatches()
-        }
-    } else {
-        alert("Saisissez un chiffre adéquat entre 1 et 6")
-        alert("Il reste " + matches + " allumettes.")
+    if(matches > 0){
         getMatches()
     }
 }
 
-function gameMatches () {
+function gameMatches (player) {
     if (matches < 0) {
-        alert("Perdu !"  + "Les allumettes ne peuvent pas être en négatif. Joueur "+ currentPlayer + " a gagné !" )
+        alert("Perdu !"  + "Les allumettes ne peuvent pas être en négatif. Joueur "+ player + " a gagné !" )
     } else if (matches === 0) {
-        alert("Félicitations joueur " + loser + " ! " + "Tu as gagné !")
-    } else {
-        getMatches()
+        alert("Félicitations joueur " + player + " ! " + "Tu as gagné !")
     }
 }
 
-gameMatches() // ✅ Lancement du jeu
+getMatches()
 
 
 
