d8ce570fa12d40dc958df007efa30eeb35d37a67
Hellyes31 <elies.auguet@gmail.com>
2025-08-28 11:55:16 +0200

    Update_starting_with_customtkinter

--- mastermind/game.py
+++ mastermind/game.py
@@ -1,4 +1,69 @@
 import random
+import customtkinter
+#INTERFACE GRAPHIQUE
+my_mastermind = customtkinter.CTk()
+my_mastermind.title("MASTERMIND")
+my_mastermind.geometry("400x800")
+
+# configuration de la grille
+my_mastermind.grid_columnconfigure(0, weight=1, uniform="a")
+my_mastermind.grid_columnconfigure(1, weight=1, uniform="a")
+my_mastermind.grid_rowconfigure(0, weight=1)
+my_mastermind.grid_rowconfigure(1, weight=0)
+my_mastermind.grid_rowconfigure(2, weight=0)
+
+
+label = customtkinter.CTkLabel(
+    my_mastermind, 
+    text="Le but de l'exercice est de deviner les 4 pions d'une couleur différentes, vous avez 12 essais pour trouver la bonne combinaison.\nVous avez le choix entre 8 couleurs: red, purple, blue, black, yellow, brown, pink, white. Attention la couleur pourrait être la même.\nBon courage",
+    anchor="center", 
+    justify="center"
+    )
+
+# sticky="new" colle et s'aligne à gauche(E) et à droite(W)
+label.grid(row=0, column=0, sticky="new", pady=(10), padx=(10))
+
+def resize_label(event):
+    label.configure(wraplength=event.width - 20)
+    
+my_mastermind.bind("<Configure>", resize_label)
+
+# Les 4 combobox en bas
+values = ["red","purple","blue","black","yellow","brown","pink","white"]
+combobox1 = customtkinter.CTkComboBox(my_mastermind, values=values)
+combobox1.grid(row=1, column=0, padx=10, pady=5, sticky="ew",)
+
+combobox2 = customtkinter.CTkComboBox(my_mastermind, values=values)
+combobox2.grid(row=1, column=1, padx=10, pady=5, sticky="ew")
+
+combobox3 = customtkinter.CTkComboBox(my_mastermind, values=values)
+combobox3.grid(row=2, column=0, padx=10, pady=5, sticky="ew")
+
+combobox4 = customtkinter.CTkComboBox(my_mastermind, values=values)
+combobox4.grid(row=2, column=1, padx=10, pady=5, sticky="ew")
+
+my_mastermind.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Enoncé du jeu
 print(f"Le but de l'exercice est de deviner les 4 pions d'une couleur différentes, vous avez 12 essais pour trouver la bonne combinaison.\nVous avez le choix entre 8 couleurs: red, purple, blue, black, yellow, brown, pink, white. Attention la couleur pourrait être la même.\nBon courage")
 answer = []
@@ -6,7 +71,7 @@ color = ["red","purple","blue","black","yellow","brown","pink","white"]
 
 def random_combinaison():
     global answer
-    answer = []  # vide la liste avant de remplir
+    answer = []
     for i in range(4):
         number_random = random.randint(0, len(color)-1)
         answer.append(color[number_random])
